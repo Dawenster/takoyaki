ed7f66cb8e24d6b6b619e07932b9b433b3960f55
David Wen <dstwen@gmail.com>
2014-12-16 08:32:59 -0800

    added letters

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,7 @@
 var app = angular.module('starter.controllers', []);
 
-app.controller('PlayCtrl', function($scope) {
-  
+app.controller('PlayCtrl', function($scope, Letters) {
+  $scope.lettersAToI = Letters.aToI();
+  $scope.lettersJToR = Letters.jToR();
+  $scope.lettersSToZ = Letters.sToZ();
 });
--- www/js/services.js
+++ www/js/services.js
@@ -7,3 +7,37 @@ app.factory("Answer", function($localstorage) {
   
 });
 
+app.factory("Letters", function() {
+  var Letters = {};
+
+  Letters.aToI = function() {
+    return ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+  }
+  
+  Letters.jToR = function() {
+    return ["J", "K", "L", "M", "N", "O", "P", "Q", "R"];
+  }
+
+  Letters.sToZ = function() {
+    return ["S", "T", "U", "V", "W", "X", "Y", "Z", ""];
+  }
+
+  return Letters
+});
+
+app.factory('$localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    }
+  }
+}])
