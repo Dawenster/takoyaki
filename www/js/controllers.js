88bfe5e56066d50c41677d69f3f1b119d2f5b167
David Wen <dstwen@gmail.com>
2014-12-16 10:04:01 -0800

    fetch game details and next phrase from api

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,4 +4,27 @@ app.controller('PlayCtrl', function($scope, Letters) {
   $scope.lettersAToI = Letters.aToI();
   $scope.lettersJToR = Letters.jToR();
   $scope.lettersSToZ = Letters.sToZ();
+
+  var root_url = "http://localhost:3000/api/";
+  // var root_url = "http://takoyaki.herokuapp.com/api/";
+  var game_details_url = root_url + "game_details";
+  var next_phrase_url = root_url + "next_phrase";
+
+  $.ajax({
+    url: game_details_url,
+    async: false
+  }).done(function(result) {
+    $scope.guesses = result.guesses;
+  }).fail(function() {
+    console.log("I'm a failure...");
+  });
+
+  $.ajax({
+    url: next_phrase_url,
+    async: false
+  }).done(function(result) {
+    $scope.phrase = jQuery.parseJSON(result.phrase);
+  }).fail(function() {
+    console.log("I'm a failure...");
+  });
 });
