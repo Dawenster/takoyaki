debc34cdc5b4100a7afb92927c80099118fbe47e
David Wen <dstwen@gmail.com>
2014-12-18 20:32:57 -0800

    remove local storage factory

--- www/js/services.js
+++ www/js/services.js
@@ -30,23 +30,6 @@ app.factory("Phrase", function() {
   return Phrase;
 });
 
-app.factory('$localstorage', ['$window', function($window) {
-  return {
-    set: function(key, value) {
-      $window.localStorage[key] = value;
-    },
-    get: function(key, defaultValue) {
-      return $window.localStorage[key] || defaultValue;
-    },
-    setObject: function(key, value) {
-      $window.localStorage[key] = JSON.stringify(value);
-    },
-    getObject: function(key) {
-      return JSON.parse($window.localStorage[key] || '{}');
-    }
-  }
-}]);
-
 app.factory('Api', function($http, $rootScope) {
   var root_url = "http://localhost:3000/api/";
   // var root_url = "http://takoyaki.herokuapp.com/api/";
